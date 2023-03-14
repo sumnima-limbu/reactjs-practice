11d608b56371f9d6dddbefff61b58fa921c3ba6a
sumnima-limbu <sumnimaklimbu@gmail.com>
2023-03-14 21:16:22 +0545

    add todo class component

---
+++ src/components/TodoClass.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+class TodoClass extends Component {
+  state = {
+    todo: "",
+    todos: [],
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      todo: event.target.value,
+    });
+  };
+
+  formSubmit = () => {
+    this.setState((prevState) => ({
+      todos: [...prevState.todos, this.state.todo],
+    }));
+
+    this.setState({
+      todo: "",
+    });
+  };
+
+  deleteMe = (value) => {
+    const filterTodos = this.state.todos.filter((element) => element !== value);
+    this.setState({
+      todos: filterTodos,
+    });
+  };
+
+  render() {
+    const { todo, todos } = this.state;
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="todo-wrapper">
+            <h1>Todo List</h1>
+            <div className="form">
+              <input
+                value={todo}
+                onChange={this.handleChange}
+                type="text"
+                id="todo"
+              />
+              <button onClick={this.formSubmit} id="addTask">
+                Add Task
+              </button>
+            </div>
+            <div className="lists">
+              {todos.map((element, i) => {
+                return (
+                  <div key={i} id="item">
+                    <p>{element}</p>
+                    <button onClick={() => this.deleteMe(element)}>
+                      Delete
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TodoClass;
