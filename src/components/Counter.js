ce701b891b3f0dee012192be6b46063302426523
sumnima-limbu <sumnimaklimbu@gmail.com>
2023-03-14 18:13:11 +0545

    add components with previous layout

---
+++ src/components/Counter.js
@@ -0,0 +1,39 @@
+import "../App.css";
+import { useState } from "react";
+
+const names = ["Arjun", "Ram", "Hari", "Bhuwan"];
+function Counter() {
+  const [name, setName] = useState("Sumnima");
+  const [counter, setCounter] = useState(0);
+
+  function clickme() {
+    console.log(name);
+    setName("Sawarloon");
+  }
+
+  const increase = () => {
+    console.log("+");
+    setCounter((prevState) => prevState + 1);
+  };
+
+  const decrease = () => {
+    console.log("-");
+    setCounter((prevState) => prevState + 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello World {name}</h1>
+      <button onClick={clickme}>Click me</button>
+      <h2>Counter: {counter}</h2>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+
+      {names.map((element) => {
+        return <p>{element}</p>;
+      })}
+    </div>
+  );
+}
+
+export default Counter;
---
+++ src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <div className="main-content">
+      <h1>Main Content</h1>
+    </div>
+  );
+};
+
+export default MainContent;
